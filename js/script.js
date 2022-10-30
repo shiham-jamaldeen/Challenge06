9f4e275ca1c53d94b4f214d06116b2a861e920aa
shiham-jamaldeen <112249220+shiham-jamaldeen@users.noreply.github.com>
2022-10-30 22:06:24 +1030

    fixed five-day forecast bug and ui tweaks (minor bug remains)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var searchByCity = document.getElementById("searchByCityContainer");
 var searchByCityBtn = document.getElementById("sendCityQuery");
 var displayContainer = document.getElementById("displayDataContainer");
+var searchHistList = document.getElementById("search-history-container");
 
 let temp = document.getElementById("id-temp");
 let wind = document.getElementById("id-wind");
@@ -18,12 +19,14 @@ let now = moment();
 const apiKey1 = "830ba8e95b985da59eb3847cf4773328";
 const apiKey2 = "47f166773e351368285402b79068ea73";
 
+//array to store city list
+const cityNameList = [];
+
 //store city name input from the user
 //URL for current weather
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 //api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-
-searchByCityBtn.addEventListener("click", function () {
+function searchForCity(event) {
   let cityName = document.getElementById("cityName").value;
 
   const apiGeoURL =
@@ -49,15 +52,17 @@ searchByCityBtn.addEventListener("click", function () {
         let cityLongitude = data[0].lon;
         //call the weather function, to display current weather details
         getCurrentWeather(cityLatitude, cityLongitude, cityName);
-        //call the five day weather function, for the same city
-        displayFiveDayWeather(cityLatitude, cityLongitude, cityName);
+
+        //storeCityList(cityName);
+        //cityNameList.push(cityName);
       } else {
         console.log("wrong city");
         window.alert("City not found. Try again");
       }
     });
-});
+}
 function getCurrentWeather(cityLatitude, cityLongitude, cityName) {
+  displayContainer.setAttribute("class", "card");
   const apiCurrentWeatherURL =
     "https://api.openweathermap.org/data/2.5/onecall?" +
     "lat=" +
@@ -80,83 +85,134 @@ function getCurrentWeather(cityLatitude, cityLongitude, cityName) {
         console.log(data);
         //weather icon
         let weatherIconURL =
-          "https://openweathermap.org/img/w/" +
+          "https://openweathermap.org/img/wn/" +
           data.current.weather[0].icon +
-          ".png";
+          "@2x.png";
         let weatherIcon = "<img src=" + weatherIconURL + ">";
-        //console.log(weatherIconURL + " " + weatherIcon);
+
         //print current weather elements to webpage
+        cityHeading.setAttribute("class", "card-header");
         cityHeading.innerHTML =
           cityName + " " + "(" + now.format("DD/MM/YYYY") + ")" + weatherIcon;
 
         temp.innerHTML =
           "Temperature: " + data.current.temp + " degrees celsius";
+
         wind.innerHTML =
           "Wind: " + data.current.wind_speed + " meters per second";
+
         humidity.innerHTML = "Humidity: " + data.current.humidity + "%";
+
         //print uvIndex and display colour coding Green, Yellow and Red based on the value
         let uvIndex = parseInt(data.current.uvi);
         if (uvIndex <= 2) {
           uvIndexDisplay.innerHTML =
-            "UV Index: " + "<span class='uv-index-low'>" + uvIndex + "</span>";
+            "UV Index: " +
+            "<span class='uv-index-low'>" +
+            uvIndex +
+            "</span>" +
+            "  " +
+            "<span class='uv-index-low'>Low</span>" +
+            "<span class='uv-index-moderate'>Moderate</span>" +
+            "<span class='uv-index-high'>High</span>" +
+            "</p>";
         } else if (uvIndex <= 5) {
           uvIndexDisplay.innerHTML =
             "UV Index: " +
             "<span class='uv-index-moderate'>" +
             uvIndex +
-            "</span>";
+            "</span>" +
+            "  " +
+            "<span class='uv-index-low'>Low</span>" +
+            "<span class='uv-index-moderate'>Moderate</span>" +
+            "<span class='uv-index-high'>High</span>" +
+            "</p>";
         } else if (uvIndex <= 10) {
           uvIndexDisplay.innerHTML =
-            "UV Index: " + "<span class='uv-index-high'>" + uvIndex + "</span>";
+            "UV Index: " +
+            "<span class='uv-index-high'>" +
+            uvIndex +
+            "</span>" +
+            "   " +
+            "<span class='uv-index-low'>Low</span>" +
+            "<span class='uv-index-moderate'>Moderate</span>" +
+            "<span class='uv-index-high'>High</span>" +
+            "</p>";
         }
+
+        //call the five day weather function, for the same city
+        displayFiveDayWeather(data);
       } else {
         console.log("Cannot display display. Please try again");
       }
     });
 }
 
-function displayFiveDayWeather(cityLatitude, cityLongitude, cityName) {
+function displayFiveDayWeather(data) {
   //some code here
   let fiveDayForecastHeading = document.getElementById(
     "displayFiveDayForecastContainer"
   );
   fiveDayForecastHeading.innerHTML =
-    "<h2 class='five-day-header'>" +
-    "5-day forecast " +
-    cityName +
-    "  " +
-    "</h2>";
+    "<h3 class='five-day-header'>" + "5-day forecast " + "</h3>";
+  let cardDeckContainer = document.getElementById("id-card-deck");
+  for (i = 1; i <= 5; i++) {
+    //create div element
+    let displayCard = document.createElement("div");
+    displayCard.setAttribute("id", i);
+    displayCard.setAttribute("class", "card");
+    cardDeckContainer.appendChild(displayCard);
+    //create img element
+    let fiveDayIconURL =
+      "https://openweathermap.org/img/wn/" +
+      data.daily[i].weather[0].icon +
+      "@2x.png";
+    let weatherIcon = document.createElement("img");
+    weatherIcon.setAttribute("src", fiveDayIconURL);
+    displayCard.appendChild(weatherIcon);
+    //create data elements
+    let forecastDate = document.createElement("p");
+    //get the date and convert it to moment.js format
+    let dateString = moment.unix(data.daily[i].dt).format("DD/MM/YYYY");
+    //print date
+    forecastDate.innerHTML = dateString;
+    forecastDate.setAttribute("class", "card-deck-body");
+    displayCard.appendChild(forecastDate);
+    //print temp
+    let fiveDayTemp = document.createElement("p");
+    fiveDayTemp.setAttribute("class", "card-deck-body");
+    fiveDayTemp.innerHTML = "Temp: " + data.daily[i].temp.day;
+    displayCard.appendChild(fiveDayTemp);
+    //print wind speed
+    let fiveDayWind = document.createElement("p");
+    fiveDayWind.setAttribute("class", "card-deck-body");
+    fiveDayWind.innerHTML = "Wind: " + data.daily[i].wind_speed;
+    displayCard.appendChild(fiveDayWind);
+    //print humidity
+    let fiveDayHumidity = document.createElement("p");
+    fiveDayHumidity.setAttribute("class", "card-deck-body");
+    fiveDayHumidity.innerHTML = "Humidity: " + data.daily[i].humidity;
+    displayCard.appendChild(fiveDayHumidity);
+  }
 
-  //Five day API URL
-  const fiveDayAPIURL =
-    "https://api.openweathermap.org/data/2.5/forecast?" +
-    "lat=" +
-    cityLatitude +
-    "&lon=" +
-    cityLongitude +
-    "&appid=" +
-    apiKey2 +
-    "&units=metric";
-  //call for 5-day
-  fetch(fiveDayAPIURL)
-    .then(function (response) {
-      if (response.ok) {
-        return response.json();
-      }
-    })
-    .then((data) => {
-      if (data.length !== 0) {
-        console.log(data);
-        //create icon URL
-
-        //let fiveDayIconURL = "https://openweathermap.org/img/w/" + data.daily[0].weather[0].icon + ".png";
-        console.log("five day weather URL: " + data.daily[0].weather[0].icon);
-      }
-    });
-
-  //make the API call, for five day
   //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 }
+function storeCityList(cityName) {
+  //heading for search hist
+
+  //push cityname to array
+  cityNameList.push(cityName);
+  //also store city in local storage, when re-opening in browser
+  localStorage.setItem("city", cityNameList);
+
+  let searchHistCity = document.createElement("button");
+  searchHistCity.innerHTML = cityName;
+  searchHistCity.setAttribute("class", "btn btn-info history-button");
+  searchHistList.appendChild(searchHistCity);
+}
+
+//main
+searchByCityBtn.addEventListener("click", searchForCity);
 //https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
 //https://www.geeksforgeeks.org/weather-app-using-vanilla-javascript/
